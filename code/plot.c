d70d77bfe9a86d4232684c57dd84343a62e9f50e
Edward2k <edulira@me.com>
2022-01-12 11:03:39 +0100

    Add plot file and methods
     - Instantiate and write to file

---
+++ code/plot.c
@@ -0,0 +1,33 @@
+#include "plot.h"
+
+int plot_init() {
+    state.step = 0;
+    state.fail = 0;
+    state.line = {0, 0, 0, 0};
+    //open file
+    state.file = fopen(FILENAME, "w");
+    if (state.file == NULL) {
+        fprintf(stderr, "Failed to instantiate the PID_value logging \n");
+        state.fail = 1;
+        return -1;
+    }
+    return 0;
+}
+
+void plot_add_point(enum DATA d, float value) {
+    if (state.fail) return;
+    state.line[d] = value;
+}
+
+void plot_draw(void) {
+    if (state.fail) return;
+    char to_write[1024];
+    int ret = fwrite(to_write, sizeof(float), 4, state.file);
+    if (ret == 0) {
+        fprintf(stderr, "Could not write to file \n");
+    }
+}
+
+void plot_deinit(void) {
+    fclose(state.file);
+}
