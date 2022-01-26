4184ea9333ad9897dd689306c12cf52256014ab5
Eduardo Lira <edulira@me.com>
2022-01-26 23:02:32 +0100

    Seperate plan and sensor IPC
     - Planning daemon decides where to put the car and updates steering
     - Throttle is read every time sensor has a new step to update RPM
     - Add deadlock prevention code
    
     - NOTES : In the current design, the time taken to plot a new throttle is not taken into account for the PID.
    This can lead to slower drops than one might expect, especially with the current aggregate window of the
    hall_effect sensor.

--- code/main.c
+++ code/main.c
@@ -14,6 +14,7 @@
 
 #include "pid.h"
 
+#define EMERGENCY_STOP_DIST (100.0f) /* CM */
 #define NS_TO_S 1000000000.0f
 #define MAX_RPM 2600.0f
 
@@ -21,11 +22,15 @@ int log_level = LOG_DEBUG | LOG_ERROR | LOG_INFO;
 
 static struct tco_shmem_data_control *shmem_control;
 static struct tco_shmem_data_plan *shmem_plan;
+static struct tco_shmem_data_sensor *shmem_sensor;
 static sem_t *shmem_sem_control;
 static sem_t *shmem_sem_plan;
+static sem_t *shmem_sem_sensor;
 static uint8_t shmem_control_open = 0;
 static uint8_t shmem_plan_open = 0;
-struct timespec timer;
+static uint8_t shmem_sensor_open = 0;
+struct timespec timer_plan;
+struct timespec timer_sensor;
 /**
  * @brief Handler for signals. This ensures that deadlocks in shmems do not occur and  when
  * clontrold is closed, control shmem is reset.
@@ -47,7 +52,13 @@ static void handle_signals(int sig)
             log_error("sem_post: %s", strerror(errno));
         }
     }
-
+    if (shmem_control_open)
+    {
+        if (sem_post(shmem_sem_control) == -1)
+        {
+            log_error("sem_post: %s", strerror(errno));
+        }
+    }
     if (sem_wait(shmem_sem_control) == -1)
     {
         log_error("sem_wait: %s", strerror(errno));
@@ -74,11 +85,11 @@ static void handle_signals(int sig)
  * Will update @p timer on each call.
  * @return float with elapsed time in seconds
  */
-static float get_elapsed_time() {
+static float get_elapsed_time(struct timespec *time_old) {
     struct timespec time_new;
     clock_gettime(_POSIX_MONOTONIC_CLOCK, &time_new);
-    float res = (time_new.tv_nsec - timer.tv_nsec)/NS_TO_S;
-    timer.tv_nsec = time_new.tv_nsec; /* Only need to update ns as s is never used. This gives more precision */
+    float res = (time_new.tv_nsec - time_old->tv_nsec)/NS_TO_S;
+    time_old->tv_nsec = time_new.tv_nsec; /* Only need to update ns as s is never used. This gives more precision */
     return res;
 }
 
@@ -107,6 +118,11 @@ int main(int argc, char const *argv[])
         log_error("Failed to map planning shmem into process memory");
         goto handle_error_and_exit;
     }
+    if (shmem_map(TCO_SHMEM_NAME_SENSOR, TCO_SHMEM_SIZE_SENSOR, TCO_SHMEM_NAME_SEM_SENSOR, O_RDONLY, (void **)&shmem_sensor, &shmem_sem_sensor) != 0)
+    {
+        log_error("Failed to map sensor shmem into process memory");
+        goto handle_error_and_exit;
+    }
 
     /* Between -1.0 and 1.0 */
     float steer_frac_raw = 0;
@@ -115,17 +131,21 @@ int main(int argc, char const *argv[])
     float target_pos = 0;
     float target_speed = 0;
     uint32_t frame_id = 0;
+    uint32_t sensor_step = 0;
     uint32_t frame_id_last = 0;
+    uint32_t sensor_step_last = 0;
     double us_1 = 0;
     double rpm = 0;
 
     /* Start time */
-    float time_elapsed = 0.0f;
-    clock_gettime(_POSIX_MONOTONIC_CLOCK, &timer);
+    float time_elapsed_plan = 0.0f;
+    float time_elapsed_sensor = 0.0f;
+    clock_gettime(_POSIX_MONOTONIC_CLOCK, &timer_sensor);
+    clock_gettime(_POSIX_MONOTONIC_CLOCK, &timer_plan);
 
     while (1)
     {
-        if (sem_wait(shmem_sem_plan) == -1)
+        if (sem_wait(shmem_sem_plan) == -1)         /* Get plan data */
         {
             log_error("sem_wait: %s", strerror(errno));
             goto handle_error_and_exit;
@@ -135,8 +155,6 @@ int main(int argc, char const *argv[])
         target_pos = shmem_plan->target_pos;
         target_speed = shmem_plan->target_speed;
         frame_id = shmem_plan->frame_id;
-	us_1 = shmem_plan->ultrasound_left;
-	rpm = shmem_plan->hall_effect_rpm;
         /* END: Critical section */
         if (sem_post(shmem_sem_plan) == -1)
         {
@@ -145,23 +163,43 @@ int main(int argc, char const *argv[])
         }
         shmem_plan_open = 0;
 
-        if (frame_id == frame_id_last)
+        if (sem_wait(shmem_sem_sensor) == -1)           /* Get sensor data */
         {
-            continue;
+            log_error("sem_wait: %s", strerror(errno));
+            goto handle_error_and_exit;
         }
-        frame_id_last = frame_id;
+        /* START: Critical section */
+        shmem_sensor_open = 1;
+        sensor_step = shmem_sensor->time_step;
+        us_1 = shmem_sensor->ultrasound_left;
+        rpm = shmem_sensor->hall_effect_rpm;
+        /* END: Critical section */
+        if (sem_post(shmem_sem_sensor) == -1)
+        {
+            log_error("sem_post: %s", strerror(errno));
+            goto handle_error_and_exit;
+        }
+        shmem_sensor_open = 0;
 
-        time_elapsed = get_elapsed_time(); /* Get time elapsed */
-        steer_frac_raw = -pid_step_steer(target_pos, 0.0f, time_elapsed);
-        //throttle_frac_raw = -pid_step_throttle(target_speed*MAX_RPM, (float)rpm, time_elapsed);
-	float desired_rpm_error = ((400.0f - (float)rpm) / MAX_RPM)*1.1;
-	
-	printf("rpm is %f error 1 is %f \n", rpm, desired_rpm_error);
-        throttle_frac_raw = -pid_step_throttle(desired_rpm_error, 0.0f, time_elapsed); 
-	printf("throttle frac raw is %f\n", throttle_frac_raw);
-	if (us_1 < 10.0f) 
-		throttle_frac_raw = -1.0f;
-        if (sem_wait(shmem_sem_control) == -1)
+        if (frame_id != frame_id_last) /* Only update steering if we got new image */
+        {
+            time_elapsed_plan = get_elapsed_time(&timer_plan); /* Get time elapsed */
+            steer_frac_raw = -pid_step_steer(target_pos, 0.0f, time_elapsed_plan);
+            frame_id_last = frame_id;
+        }
+
+        if (sensor_step != sensor_step_last) { /* Update the motor RPM everytime we get new reading */
+            time_elapsed_sensor = get_elapsed_time(&timer_sensor); /* Get time elapsed */
+            float desired_rpm = ((target_speed + 1)/2.0f) * MAX_RPM;
+	        float desired_rpm_error = ((desired_rpm - (float)rpm) / MAX_RPM);
+            throttle_frac_raw = -pid_step_throttle(desired_rpm_error, 0.0f, time_elapsed_sensor);
+            sensor_step_last = sensor_step;
+        }
+
+        if (us_1 < EMERGENCY_STOP_DIST) /* Emergency stop */
+            throttle_frac_raw = -1.0f;
+
+        if (sem_wait(shmem_sem_control) == -1) /* Write to control shmem for actuation */
         {
             log_error("sem_wait: %s", strerror(errno));
             goto handle_error_and_exit;
